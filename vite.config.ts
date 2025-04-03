bab45378c743bdafe37f9641c2659ed73b1498ea
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2025-04-03 13:40:12 -0600

    feat(): Adición de un Readme personalizado

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/*',
+                    dest: 'assets'
+                }
+            ]
+        })
     ],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
 })
